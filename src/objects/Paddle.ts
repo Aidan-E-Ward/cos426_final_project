4e99c58aeff23496f7a82d3d5662f718b7fd9c07
Vincent Etherton <vincent.etherton@gmail.com>
2024-12-10 13:37:21 -0500

    Made more progress on paddle

--- src/objects/Paddle.ts
+++ src/objects/Paddle.ts
@@ -1,4 +1,4 @@
-import { Group, Mesh } from 'three';
+import { Group, Object3D } from 'three';
 // import { Vector3 } from 'three';
 import SeedScene from '../scenes/SeedScene';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
@@ -10,8 +10,9 @@ import MODEL from './paddle.gltf?url';
 import C from 'cannon';
 
 class Pinball extends Group {
-    paddle_body: C.Body;
-    paddle_mesh = Mesh;
+    paddle_body;
+    paddle_mesh;
+    paddle: Object3D; // am i dumb why is this not "definitely assigned"
 
     constructor(parent: SeedScene) {
         super();
@@ -19,14 +20,34 @@ class Pinball extends Group {
         // Load object
         const loader = new GLTFLoader();
 
+
+        // Example from lightsaber project: https://github.com/arcturus3/lightsaber-dojo/blob/master/src/Lightsaber.ts
+
+        // this.mesh = new Group();
+        // this.name = 'LightSaber';
+        // loader.load('models/obi-wan_kenobi_lightsaber/scene.gltf', (gltf) => {
+        //     gltf.scene.scale.divideScalar(50);
+        //     // gltf.scene.scale.y *= 30;
+        //     gltf.scene.rotateY(degToRad(60));
+        //     gltf.scene.rotateZ(degToRad(10));
+
+        //     gltf.scene.rotation.x = degToRad(6.5);
+        //     gltf.scene.position.z -= 0.005;
+        //     gltf.scene.position.y -= 0.1;
+
+        //     // console.log(gltf.scene.worldToLocal(new Vector3(1,0,0)));
+        //     // console.log(gltf.scene.rotation);
+        //     this.hilt = gltf.scene.clone();
+        //     this.mesh.add(this.hilt);
+        
+        // });
+
+        this.paddle_mesh = new Group();
         this.name = 'paddle';
         loader.load(MODEL, (gltf) => {
-            gltf.scene;
-            // gltf.scene.traverse((child: Mesh) => {
-            //     this.paddle_mesh = child;
-            //     this.paddle_mesh.position.set(new Vector3(0,0,0));
-            //     this.add(this.paddle_mesh);
-            // });
+            gltf.scene.position.set(0,0,0);
+            this.paddle = gltf.scene.clone();
+            this.paddle_mesh.add(this.paddle);
         });
 
         this.paddle_body = new C.Body({
